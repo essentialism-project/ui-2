d4657fd45d4697c99dea204ba2cbe9f6773dbbe6
Lisa <lmkirk@gmail.com>
2019-06-26 10:06:09 -0400

    testimonials skeleton with background

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,3 @@
-
 class TabLink {
     constructor(element) {
         // Assign this.element to the passed in DOM element
@@ -64,13 +63,9 @@ class TabItem {
 }
 
 /* START HERE:
-
 - Select all classes named ".tabs-link" and assign that value to the links variable
-
 - With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
-
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
-
 */
 
 const links = document.querySelectorAll('.tabs-link');
